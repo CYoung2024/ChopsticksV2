b8e669ad3341c450a02abbb045a57c2f9f32ba91
Charlie Young <46425874+Goalinetsky@users.noreply.github.com>
2023-03-16 11:57:35 -0400

    arrows implemented

--- chopsticks.py
+++ chopsticks.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from math import atan2, sin, cos, pi
 
 
 class DropDown():
@@ -44,6 +45,34 @@ class DropDown():
                 self.draw_menu = False
                 return self.active_option
         return -1
+
+
+class Arrow():
+    # base and point are (x,y) locations
+    def __init__(self, base:tuple):
+        self.base = base
+        self.point = base
+        self.end = base
+        self.theta = 0
+        self.p1 = base
+        self.p2 = base
+        self.a = 35 # triangle height
+        self.b = 15 # half triangle base
+
+    def draw(self, surf):
+        pg.draw.line(surf, (0,0,0), self.base, self.end, width=10)
+        pg.draw.polygon(surf, (0,0,0), [self.point, self.p1, self.p2])
+
+    def update(self):
+        self.point = pg.mouse.get_pos()
+        self.theta = atan2(self.point[1]-self.base[1], self.point[0]-self.base[0])
+        self.p1 = (self.point[0]-self.a*cos(self.theta)+self.b*cos(self.theta-pi/2), self.point[1]-self.a*sin(self.theta)+self.b*sin(self.theta-pi/2))
+        self.p2 = (self.point[0]-self.a*cos(self.theta)-self.b*cos(self.theta-pi/2), self.point[1]-self.a*sin(self.theta)-self.b*sin(self.theta-pi/2))
+        self.end = (self.point[0]-self.a/2*cos(self.theta), self.point[1]-self.a/2*sin(self.theta))
+
+
+
+
 
 
 # what happens when one team hits other team
@@ -107,13 +136,21 @@ def main():
     50, 300, 250, 50, medFont, "Select Who Starts", ["Player", "Computer"])
     optionsPlayButtonText = bigFont.render('Play' , True , (0,0,0))
 
+    botRightRect = pg.Rect(154,96,112,105)
+    botLeftRect = pg.Rect(374,96,112,105)
+    playerLeftRect = pg.Rect(154,296,112,105)
+    playerRightRect = pg.Rect(374,296,112,105)
+
+
     running = True
-    screenState = "welcome"
-    turn = ""; gamemode = ""
-    playerVal1 = 1; playerVal2 = 1
-    botVal1 = 1; botVal2 = 1
+    screenState = 'welcome'
+    turn = ''; gamemode = ''
+    playerVal1 = 5; playerVal2 = 2
+    botVal1 = 3; botVal2 = 4
     playerLeftAlive = True; playerRightAlive = True
     botRightAlive = True; botLeftAlive = True
+    arrowActive = False; splitActive = False
+    handSelected = 'none'; target = 'none'
 
     ### Game loop
     while running:
@@ -128,25 +165,79 @@ def main():
                 pg.quit()
                 raise SystemExit
             mouse = pg.mouse.get_pos() # stores (x,y) mouse position as a tuple
-            if event.type == pg.MOUSEBUTTONUP:
-                print(mouse) # for finding positions and debugging
+            #if event.type == pg.MOUSEBUTTONUP:
+                #print(mouse) # for finding positions and debugging
             ## On welcome screen
             if screenState == "welcome":
-                if event.type == pg.MOUSEBUTTONUP:
-                    if 50 <= mouse[0] <= 50+232 and 380 <= mouse[1] <= 380+45:
-                        screenState = "options"
+                if event.type == pg.MOUSEBUTTONUP and 50 <= mouse[0] <= 50+232 and 380 <= mouse[1] <= 380+45:
+                    screenState = "options"
             ## On options screen
             if screenState == "options":
                 selectedOption = gamemodeList.update(event)
                 selectedWhoStarts = whoStartsList.update(event)
-                if event.type == pg.MOUSEBUTTONUP:
-                    if 495 <= mouse[0] <= 495+95 and 395 <= mouse[1] <= 395+55:
-                        screenState = "playing"
-                        turn = whoStartsList.main
-                        gamemode = gamemodeList.main
+                if event.type == pg.MOUSEBUTTONUP and 495 <= mouse[0] <= 495+95 and 395 <= mouse[1] <= 395+55:
+                    screenState = "playing"
+                    turn = whoStartsList.main
+                    gamemode = gamemodeList.main
             ## On gameplay screen, player's turn
             if screenState == "playing" and turn == 'Player':
-                pass
+                # player's first mouse down to create arrow
+                if event.type == pg.MOUSEBUTTONDOWN and not arrowActive and not splitActive:
+                    # left hand selected
+                    if playerLeftRect.collidepoint(mouse):
+                        arrow = Arrow(mouse)
+                        arrowActive = True
+                        handSelected = 'playerLeft'
+                    # right hand selected
+                    elif playerRightRect.collidepoint(mouse):
+                        arrow = Arrow(mouse)
+                        arrowActive = True
+                        handSelected = 'playerRight'
+                # player's second mouse down to place arrow
+                elif event.type == pg.MOUSEBUTTONDOWN and arrowActive and not splitActive:
+                    # left hand selected
+                    if handSelected == 'playerLeft':
+                        if playerLeftRect.collidepoint(mouse):
+                            target = 'none'
+                            handSelected = 'none'
+                            arrowActive = False
+                        if playerRightRect.collidepoint(mouse): 
+                            target = 'playerRight'
+                            arrowActive = False
+                        if botRightRect.collidepoint(mouse): 
+                            target = 'botRight'
+                            arrowActive = False
+                        if botLeftRect.collidepoint(mouse):
+                            target = 'botLeft'
+                            arrowActive = False
+                    # right hand selected
+                    elif handSelected == 'playerRight':
+                        if playerLeftRect.collidepoint(mouse):
+                            target = 'playerLeft'
+                            arrowActive = False
+                        if playerRightRect.collidepoint(mouse): 
+                            target = 'none'
+                            handSelected = 'none'
+                            arrowActive = False
+                        if botRightRect.collidepoint(mouse): 
+                            target = 'botRight'
+                            arrowActive = False
+                        if botLeftRect.collidepoint(mouse):
+                            target = 'botLeft'
+                            arrowActive = False
+                # player moves arrow
+                if event.type == pg.MOUSEMOTION and arrowActive and not splitActive:
+                    arrow.update()
+            ## On gameplay screen, computer's turn
+            if screenState == "playing" and turn == "Computer":
+                print('Computer takes turn')
+                turn = 'player'
+
+                
+                
+
+                
+                
 
 
         ### Update Graphics
@@ -172,16 +263,17 @@ def main():
 
         if screenState == "playing":
             pg.Surface.fill(screen, (255,255,255))
-            pg.draw.rect(screen, (0,0,0), (158,100,104,98), width=3)
-            pg.draw.rect(screen, (0,0,0), (158,300,104,98), width=3)
-            pg.draw.rect(screen, (0,0,0), (378,100,104,98), width=3)
-            pg.draw.rect(screen, (0,0,0), (378,300,104,98), width=3)
+            pg.draw.rect(screen, (0,0,0), botRightRect, width=3)
+            pg.draw.rect(screen, (0,0,0), playerLeftRect, width=3)
+            pg.draw.rect(screen, (0,0,0), botLeftRect, width=3)
+            pg.draw.rect(screen, (0,0,0), playerRightRect, width=3)
             screen.blit(playerLeft[playerVal1], playerLeft[playerVal1].get_rect(center = (158+104/2, 300+98/2)))
             screen.blit(playerRight[playerVal2], playerRight[playerVal2].get_rect(center = (378+104/2, 300+98/2)))
             screen.blit(botRight[botVal1], botRight[botVal1].get_rect(center = (158+104/2, 100+98/2)))
             screen.blit(botLeft[botVal2], botLeft[botVal2].get_rect(center = (378+104/2, 100+98/2)))
         if screenState == "playing" and turn == "Player":
-            pass
+            if arrowActive:
+                arrow.draw(screen)
 
 
         pg.display.flip()
